733cfeaa0ca7a6a8dfed75419747edbcb7519734
preetam potadar <preetamnp@gmail.com>
2019-10-25 14:57:27 +0200

    first commit

---
+++ src/main/java/com/investment/advice/client/InvestmentAdviceClient/InvestmentAdviceClientApplication.java
@@ -0,0 +1,13 @@
+package com.investment.advice.client.InvestmentAdviceClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentAdviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentAdviceClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investment/advice/client/InvestmentAdviceClient/InvestmentAdviceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment.advice.client.InvestmentAdviceClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAdviceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
